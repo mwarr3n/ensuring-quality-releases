be6a665f41bae287c2590d15768761ed9519b487
Michael W <mwarr3n@gmail.com>
2021-11-02 10:18:20 -0500

    update

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -4,18 +4,19 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
 
 def manage_cart (inventory_items, action):
-	
-	for inventory_item in inventory_items:
-		item_name = inventory_item.find_element(By.CLASS_NAME, "inventory_item_label").find_element(By.CLASS_NAME, "inventory_item_name").text
 
-		price_bar = inventory_item.find_element(By.CLASS_NAME, "pricebar")
+    for inventory_item in inventory_items:
+        item_name = inventory_item.find_element(By.CLASS_NAME, "inventory_item_label").find_element(By.CLASS_NAME, "inventory_item_name").text
 
-		if action == "Add":
-			print("Adding items to cart")
-		else: 
-			print("Removing items from cart")
-		print("    " + item_name)
-		price_bar.find_element(By.CLASS_NAME, "btn_inventory").click()
+        price_bar = inventory_item.find_element(By.CLASS_NAME, "pricebar")
+
+        if action == "Add":
+            print("Adding items to cart")
+        else: 
+            print("Removing items from cart")
+
+        print("    " + item_name)
+        price_bar.find_element(By.CLASS_NAME, "btn_inventory").click()
 	
 	
 # Start the browser and login with standard_user
