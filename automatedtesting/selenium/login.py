cdd6dfe600e8bada7ae5024c94acd45de2f6f907
mwarr3n <mwarr3n@users.noreply.github.com>
2021-11-02 08:32:11 -0500

    Update login.py

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -3,48 +3,56 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
 
-def manage_cart (inventory_items, action):
-								 
-	for inventory_item in inventory_items:
-		item_name = inventory_item.find_element(By.CLASS_NAME, "inventory_item_label").find_element(By.CLASS_NAME, "inventory_item_name").text
-		print("Selecting Item: " + item_name)
-
-		price_bar = inventory_item.find_element(By.CLASS_NAME, "pricebar")
-
-		print(action + "item " + item_name)
-		price_bar.find_element(By.CLASS_NAME, "btn_inventory").click()
-		print("------")
-		
 # Start the browser and login with standard_user
 def login (user, password):
-	print ('Starting the browser...')
-	# --uncomment when running in Azure DevOps.
-	options = ChromeOptions()
-	options.add_argument("--headless") 
-	driver = webdriver.Chrome(options=options)
-	#     driver = webdriver.Chrome()
-	print ('Browser started successfully. Navigating to the demo page to login.')
-	driver.get('https://www.saucedemo.com/')
+    print ('Starting the browser...')
+    # --uncomment when running in Azure DevOps.
+    options = ChromeOptions()
+    options.add_argument("--headless") 
+    driver = webdriver.Chrome(options=options)
+#     driver = webdriver.Chrome()
+    print ('Browser started successfully. Navigating to the demo page to login.')
+    driver.get('https://www.saucedemo.com/')
+    
+    driver.find_element(By.CSS_SELECTOR, "input[id='user-name']").send_keys(user)
+    driver.find_element(By.CSS_SELECTOR, "input[id='password']").send_keys(password)
+    driver.find_element(By.ID, "login-button").click()
 
-	driver.find_element(By.CSS_SELECTOR, "input[id='user-name']").send_keys(user)
-	driver.find_element(By.CSS_SELECTOR, "input[id='password']").send_keys(password)
-	driver.find_element(By.ID, "login-button").click()
+    print(" User " + user + " logged in successfully!")
+    
+    inventory_items = driver.find_elements(By.CLASS_NAME, "inventory_item")
+    print("getting inventory items")
+    print("Items found: " + str(len(inventory_items)))
 
-	logout = driver.find_element(By.ID, 'logout_sidebar_link'.size() > 0
+    if inventory_items:    
+        for inventory_item in inventory_items:
+            item_name = inventory_item.find_element(By.CLASS_NAME, "inventory_item_label").find_element(By.CLASS_NAME, "inventory_item_name").text
+            print("Found Item Name: " + item_name)
 
-	inventory_items = driver.find_elements(By.CLASS_NAME, "inventory_item")
-	print("getting inventory items")
-	print("Items found: " + str(len(inventory_items)))
+            price_bar = inventory_item.find_element(By.CLASS_NAME, "pricebar")
 
-# 	if inventory_items:
-# 		manage_cart(inventory_items,'Add')
+            print("adding item " + item_name)
+            add_to_cart = price_bar.find_element(By.CLASS_NAME, "btn_inventory").click()
+            print("------")
+        
+        cart_badge = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge').text
+        print("Items in cart " + cart_badge)
+        print("All items have been added")
+        print("******")
+        
+        for inventory_item in inventory_items:
+            item_name = inventory_item.find_element(By.CLASS_NAME, "inventory_item_label").find_element(By.CLASS_NAME, "inventory_item_name").text
+            print("Found Item Name: " + item_name)
 
-# 	cart_badge = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge').text
-# # 	assert cart_badge == len(inventory_items) 
-# # 	print('All Items added to cart')
+            price_bar = inventory_item.find_element(By.CLASS_NAME, "pricebar")
 
-# 	manage_cart(inventory_items,'Remove')
+            print("removing item " + item_name)
+            add_to_cart = price_bar.find_element(By.CLASS_NAME, "btn_inventory").click()
+            print("------")
+        
+        cart_badge = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge').text
+        print("Items in cart " + cart_badge)
+        print("All items have been removed")
+        print("******")
 
-# 	cart_badge = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge'.size() > 0
-# # 	assert cart_badge == False, 'All Items removed to cart'
-
+login('standard_user', 'secret_sauce')
