0baf82d9f5c8fd7c3c3a554cbef89ae19ad419a9
mwarr3n <mwarr3n@users.noreply.github.com>
2021-11-02 07:19:43 -0500

    Update login.py

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -3,6 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
 
+def process_inventory_items(inventory_items, action):
+    
+
 # Start the browser and login with standard_user
 def login (user, password):
     print ('Starting the browser...')
@@ -20,40 +23,73 @@ def login (user, password):
 
     print(" User " + user + " logged in successfully!")
     
+#     inventory_items = driver.find_elements(By.CLASS_NAME, "inventory_item")
+#     print("getting inventory items")
+#     print("Items found: " + str(len(inventory_items)))
+
+#     if inventory_items:    
+#         for inventory_item in inventory_items:
+#             item_name = inventory_item.find_element(By.CLASS_NAME, "inventory_item_label").find_element(By.CLASS_NAME, "inventory_item_name").text
+#             print("Found Item Name: " + item_name)
+
+#             price_bar = inventory_item.find_element(By.CLASS_NAME, "pricebar")
+
+#             print("adding item " + item_name)
+#             add_to_cart = price_bar.find_element(By.CLASS_NAME, "btn_inventory").click()
+#             print("------")
+        
+#         cart_badge = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge').text
+#         print("Items in cart " + cart_badge)
+#         print("All items have been added")
+#         print("******")
+        
+#         for inventory_item in inventory_items:
+#             item_name = inventory_item.find_element(By.CLASS_NAME, "inventory_item_label").find_element(By.CLASS_NAME, "inventory_item_name").text
+#             print("Found Item Name: " + item_name)
+
+#             price_bar = inventory_item.find_element(By.CLASS_NAME, "pricebar")
+
+#             print("removing item " + item_name)
+#             add_to_cart = price_bar.find_element(By.CLASS_NAME, "btn_inventory").click()
+#             print("------")
+        
+#         cart_badge = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge').text
+#         print("Items in cart " + cart_badge)
+#         print("All items have been removed")
+#         print("******")
+
+# login('standard_user', 'secret_sauce')
+
+def manage_cart(inventory_items, action):
+    for inventory_item in inventory_items:
+        item_name = inventory_item.find_element(By.CLASS_NAME, "inventory_item_label").find_element(By.CLASS_NAME, "inventory_item_name").text
+        print("Selecting Item: " + item_name)
+
+        price_bar = inventory_item.find_element(By.CLASS_NAME, "pricebar")
+
+        print(action + "item " + item_name)
+        price_bar.find_element(By.CLASS_NAME, "btn_inventory").click()
+        print("------")
+        
+        if action = "Add":
+            cart_badge = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge').text
+            assert cart_badge == len(inventory_items), 'All Items added to cart'
+        else:
+            
+
+def main():
+    driver = login('standard_user', 'secret_sauce')
+    
     inventory_items = driver.find_elements(By.CLASS_NAME, "inventory_item")
     print("getting inventory items")
     print("Items found: " + str(len(inventory_items)))
-
-    if inventory_items:    
-        for inventory_item in inventory_items:
-            item_name = inventory_item.find_element(By.CLASS_NAME, "inventory_item_label").find_element(By.CLASS_NAME, "inventory_item_name").text
-            print("Found Item Name: " + item_name)
-
-            price_bar = inventory_item.find_element(By.CLASS_NAME, "pricebar")
-
-            print("adding item " + item_name)
-            add_to_cart = price_bar.find_element(By.CLASS_NAME, "btn_inventory").click()
-            print("------")
+    
+    if inventory_items:
+        manage_cart(inventory_items,'Add')
         
-        cart_badge = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge').text
-        print("Items in cart " + cart_badge)
-        print("All items have been added")
-        print("******")
-        
-        for inventory_item in inventory_items:
-            item_name = inventory_item.find_element(By.CLASS_NAME, "inventory_item_label").find_element(By.CLASS_NAME, "inventory_item_name").text
-            print("Found Item Name: " + item_name)
+        manage_cart(inventory_items,'Remove')
+    
 
-            price_bar = inventory_item.find_element(By.CLASS_NAME, "pricebar")
-
-            print("removing item " + item_name)
-            add_to_cart = price_bar.find_element(By.CLASS_NAME, "btn_inventory").click()
-            print("------")
-        
-        cart_badge = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge').text
-        print("Items in cart " + cart_badge)
-        print("All items have been removed")
-        print("******")
-
-login('standard_user', 'secret_sauce')
+if __name__ == '__main__':
+	main()
 
