9d60343c6dd99211266ebbf7669d9f60e858d186
mwarr3n <mwarr3n@users.noreply.github.com>
2021-11-01 21:00:58 -0500

    Update login.py

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -1,6 +1,7 @@
 # #!/usr/bin/env python
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.common.by import By
 
 
 # Start the browser and login with standard_user
@@ -14,18 +15,18 @@ def login (user, password):
     print ('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
     
-    driver.find_element_by_css_selector("input[id='user-name']").send_keys(user)
-    driver.find_element_by_css_selector("input[id='password']").send_keys(password)
-    driver.find_element_by_css_selector("login-button").click()
+    driver.find_element(By.CSS_SELECTOR, "input[id='user-name']").send_keys(user)
+    driver.find_element(By.CSS_SELECTOR, "input[id='password']").send_keys(password)
+    driver.find_element(By.CSS_SELECTOR, "login-button").click()
 
     print(" User " + user + " logged in successfully!")
     
-    inventory_items = driver.find_element_by_css_selector("inventory_list")
-    print("getting inventory items")
+#     inventory_items = driver.find_element_by_css_selector("inventory_list")
+#     print("getting inventory items")
     
-    for inventory_item in inventory_items:
-        item_name = inventory_item.find_element_by_class_name("inventory_item_label").find_element_by_class_name("inventory_item_name").text
-        print(item_name)
+#     for inventory_item in inventory_items:
+#         item_name = inventory_item.find_element(By.CLASS_NAME, "inventory_item_label").find_element(By.CLASS_NAME, "inventory_item_name").text
+#         print(item_name)
 
     
 login('standard_user', 'secret_sauce')
